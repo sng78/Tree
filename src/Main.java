6c2598276de3ca695810c6d1fd661c7c6353488a
Sergei Gorbachev <sng78@list.ru>
2022-09-09 21:49:55 +0300

    use lambda & removing under age of 18

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+//import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> person = new ArrayList<>();
         person.add(new Person("Vasilij", "Vasiliev", 8));
-        person.add(new Person("Petr", "Petrov", 8));
+        person.add(new Person("Petr", "Petrov", 19));
         person.add(new Person("Sergei", "Sergeev", 18));
         person.add(new Person("Victor", "Victorov", 10));
 
-        Collections.sort(person, new PersonsNobilityComparator(2));
+        person.removeIf(p -> p.getAge() < 18);
         System.out.println(person);
-        Collections.sort(person, new PersonsNobilityComparator(7));
+
+        /*/
+        Predicate<Person> condition = p -> p.getAge() < 18;
+        person.removeIf(condition);
         System.out.println(person);
-        Collections.sort(person, new PersonsNobilityComparator(8));
-        System.out.println(person);
+         */
     }
 }
